d9a9cfc61ef573cd2491288f5895e625145539e1
Federico Cara <fcara.utn@gmail.com>
2024-08-20 00:37:54 -0300

    Multiplier fix

--- Assets/Scripts/Model/MultiplicationModifier.cs
+++ Assets/Scripts/Model/MultiplicationModifier.cs
@@ -6,6 +6,6 @@ namespace Model
     [CreateAssetMenu(menuName = "AlchemyScale/Multiplication Modifier")]
     public class MultiplicationModifier : Ingredient
     {
-        public override int CalculateWeight(List<Ingredient> previousIngredients) => 0;
+        public override int CalculateWeight(List<Ingredient> previousIngredients) => Weight;
     }
 }
